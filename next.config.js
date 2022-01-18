fcf3456db2cf8a241d4f6e113223ef259e4c858e
Chan Nyein Tun <channyeintun@yahoo.com>
2022-01-18 13:32:36 +0630

    added x-frame-options

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 module.exports = {
       async headers() {
-        return [
-          {
-            // matching all API routes
-            source: "/downloads/:path*",
-            headers: [
+            return [
                   {
-                        "key": "Cross-Origin-Opener-Policy",
-                        "value": "same-origin"
-                  },
-                  {
-                        "key": "Cross-Origin-Embedder-Policy",
-                        "value": "require-corp"
+                        // matching all API routes
+                        source: "/downloads/:path*",
+                        headers: [
+                              {
+                                    "key": "Cross-Origin-Opener-Policy",
+                                    "value": "same-origin"
+                              },
+                              {
+                                    "key": "Cross-Origin-Embedder-Policy",
+                                    "value": "require-corp"
+                              },
+                              {
+                                    key: 'X-Frame-Options',
+                                    value: 'SAMEORIGIN'
+                              }
+                        ]
                   }
             ]
-          }
-        ]
       }
-    };
+};
